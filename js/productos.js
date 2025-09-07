e50c4759991b9774a83e45d9d4cf0cb726ddf1a8
btnvlscoder <btnvlscoder@gmail.com>
2025-09-06 22:13:32 -0400

    Se agrega lista de productos como  productos.js

---
+++ js/productos.js
@@ -0,0 +1,92 @@
+//lista de productos
+const productos = [
+    {
+        id: "JM001",
+        categoria: "Juegos de Mesa",
+        nombre: "Catan",
+        precio: 29990,
+        descripcion: "Un clásico juego de estrategia donde los jugadores compiten por colonizar y expandirse en la isla de Catan. Ideal para 3-4 jugadores y perfecto para noches de juego en familia o con amigos.",
+        img: "img/productos/catan/1.jpg"
+    },
+
+    {
+        id: "JM002",
+        categoria: "Juegos de Mesa",
+        nombre: "Carcassonne",
+        precio: 24990,
+        descripcion: "Un juego de colocación de fichas donde los jugadores construyen el paisaje alrededor de la fortaleza medieval de Carcassonne. Ideal para 2-5 jugadores y fácil de aprender.",
+        img: "img/productos/carcassonne/1.jpg"
+    },
+    
+    {
+        id: "AC001",
+        categoria: "Accesorios",
+        nombre: "Controlador Inalámbrico Xbox Series X",
+        precio: 0,
+        descripcion: "Ofrece una experiencia de juego cómoda con botones mapeables y una respuesta táctil mejorada. Compatible con consolas Xbox y PC.",
+        img: "img/productos/Controlador Inalámbrico Xbox Series X/1.jpg"
+    },
+    
+    {
+        id: "AC002",
+        categoria: "Accesorios",
+        nombre: "Auriculares Gaming HyperX",
+        precio: 79990,
+        descripcion: " Proporcionan un sonido envolvente de calidad con un micrófono desmontable y almohadillas de espuma viscoelástica para mayor comodidad durante largas sesiones de juego.",
+        img: "img/productos/Auriculares Gaming HyperX/1.jpg"
+    },
+
+    {
+        id: "CO001",
+        categoria: "Consolas",
+        nombre: "PlayStation 5",
+        precio: 549990,
+        descripcion: "La consola de última generación de Sony, que ofrece gráficos impresionantes y tiempos de carga ultrarrápidos para una experiencia de juego inmersiva.",
+        img: "img/PlayStation 5/1.jpg"
+    },
+    
+    {
+        id: "CG001",
+        categoria: "Computadores Gamers",
+        nombre: "PC Gamer ASUS ROG Strix",
+        precio: 1299990,
+        descripcion: "Un potente equipo diseñado para los gamers más exigentes, equipado con los últimos componentes para ofrecer un rendimiento excepcional en cualquier juego.",
+        img: "img/PC Gamer ASUS ROG Strix/1.jpg"
+    },
+    
+    {
+        id: "SG001",
+        categoria: "Sillas Gamers",
+        nombre: "Silla Gamer Secretlab Titan",
+        precio: 349990,
+        descripcion: "Diseñada para el máximo confort, esta silla ofrece un soporte ergonómico y personalización ajustable para sesiones de juego prolongadas.",
+        img: "img/Silla Gamer Secretlab Titan/1.jpg"
+    },
+    
+    {
+        id: "MS001",
+        categoria: "Mouse",
+        nombre: "Mouse Gamer Logitech G502 HERO",
+        precio: 49990,
+        descripcion: "Con sensor de alta precisión y botones personalizables, este mouse es ideal para gamers que buscan un control preciso y personalización.",
+        img: "img/Mouse Gamer Logitech G502 HERO/1.jpg"
+    },
+    
+    {
+        id: "MP001",
+        categoria: "Mousepad",
+        nombre: "Mousepad Razer Goliathus Extended Chroma",
+        precio: 29990,
+        descripcion: "Ofrece un área de juego amplia con iluminación RGB personalizable, asegurando una superficie suave y uniforme para el movimiento del mouse.",
+        img: "img/Mousepad Razer Goliathus Extended Chroma/1.jpg"
+    },
+    
+    {
+        id: "PP001",
+        categoria: "Poleras Personalizadas",
+        nombre: "Polera Gamer Personalizada 'Level-Up'",
+        precio: 14990,
+        descripcion: "Una camiseta cómoda y estilizada, con la posibilidad de personalizarla con tu gamer tag o diseño favorito.",
+        img: "img/Poleras Personalizadas/1.jpg"
+    }
+]
